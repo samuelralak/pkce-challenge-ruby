e8402600655fae05dfa2c5de8ed9de1e3e97bba1
Samuel Ralak <thesamuelralak@gmail.com>
2019-11-18 06:15:20 +0300

    documented the challenge method

--- lib/pkce_challenge.rb
+++ lib/pkce_challenge.rb
@@ -6,6 +6,20 @@ require "pkce_challenge/challenge"
 module PkceChallenge
   class LengthOutOfRangeError < StandardError; end
 
+  # Generate code challenge and verfiier
+  #
+  # Example:
+  #   >> PkceChallenge.challenge
+  #   => #<PkceChallenge::Challenge:0x00007f894f810378 @char_length=48, @code_verifier="QbS08cDO9pce~HVCKe9-UDiJoBMG8xwql4FI.Y3CIdpyJtPU", @code_challenge="HT90mmypkXgneRUVK-Ja009VvnoL-flydbEgRcTp5Yw">
+  #
+  # == Parameters:
+  # options::
+  #   A Hash containing optional arguments. Supported options
+  #   can include `:char_length`
+  #
+  # == Returns:
+  # An instance of PkceChallenge::Challenge
+  #
   def self.challenge(options = {})
     PkceChallenge::Challenge.new(options)
   end
