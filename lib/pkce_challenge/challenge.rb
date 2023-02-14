e2e83309f2bfe8b8f0fd54e54f55e038e22bb0a2
Minhajuddin Khaja <minhajuddin.k@gmail.com>
2023-02-14 13:09:45 -0500

    Use Ruby Base class functions for encoding
    This PR uses the Base64 ruby base class to encode the code verifier and
    make the code simpler.

--- lib/pkce_challenge/challenge.rb
+++ lib/pkce_challenge/challenge.rb
@@ -1,23 +1,12 @@
 # frozen_string_literal: true
 
+require 'base64'
 require "digest"
 require "securerandom"
 
 module PkceChallenge
   class Challenge
-    attr_reader :verifier, :code_challenge
-
-    def initialize(options = {})
-      @options = options
-    end
-
-    def code_verifier
-      @verifier ||= generate_code_verifier
-    end
-
-    def code_challenge
-      @code_challenge ||= generate_pkce_challenge
-    end
+    attr_reader :verifier, :code_challenge, :code_verifier
 
     # constants definition
 
@@ -27,24 +16,18 @@ module PkceChallenge
       min: 43,
     }.freeze
 
-    private
-
-    def generate_code_verifier
-      length = (@options[:char_length] || CHAR_LENGTH[:default]).to_i
+    def initialize(options = {})
+      length = (options[:char_length] || CHAR_LENGTH[:default]).to_i
 
       unless length.between?(CHAR_LENGTH[:min], CHAR_LENGTH[:max])
         raise PkceChallenge::LengthOutOfRangeError
       end
 
-      urlsafe_base64(SecureRandom.base64((length * 3) / 4))
-    end
-
-    def generate_pkce_challenge
-      urlsafe_base64(Digest::SHA256.base64digest(code_verifier))
-    end
-
-    def urlsafe_base64(base64_str)
-      base64_str.tr("+/", "-_").tr("=", "")
+      random_bytes = SecureRandom.random_bytes(length)
+      # NOTE: we set both code_verifier and verifier to the same value
+      # to keep backward compatibility
+      @code_verifier = @verifier = Base64.urlsafe_encode64(random_bytes, padding: false)
+      @code_challenge = Base64.urlsafe_encode64(Digest::SHA256.digest(code_verifier), padding: false)
     end
   end
 end
--- spec/pkce_challenge_spec.rb
+++ spec/pkce_challenge_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "spec_helper"
+require 'base64'
 
 RSpec.describe PkceChallenge do
   describe "#challenge" do
@@ -11,21 +12,20 @@ RSpec.describe PkceChallenge do
     end
 
     it "should have verifier with default char length" do
-      expect(challenge.code_verifier.length).to equal(48)
+      expect(Base64.urlsafe_decode64(challenge.code_verifier).length).to equal(48)
     end
 
     it "should match ABNF described in [RFC 7636](https://tools.ietf.org/html/rfc7636)" do
       max_length_challenge = PkceChallenge.challenge(char_length: 128)
       min_length_challenge = PkceChallenge.challenge(char_length: 43)
 
-      expect(challenge.code_verifier).to match(/^([A-Za-z0-9]|\-|\.|_|~){43,128}$/)
-      expect(max_length_challenge.code_verifier).to match(/^([A-Za-z0-9]|\-|\.|_|~){43,128}$/)
-      expect(min_length_challenge.code_verifier).to match(/^([A-Za-z0-9]|\-|\.|_|~){43,128}$/)
+      expect(challenge.code_verifier).to match(/^([A-Za-z0-9]|\-|\.|_|~)+$/)
+      expect(max_length_challenge.code_verifier).to match(/^([A-Za-z0-9]|\-|\.|_|~)+$/)
+      expect(min_length_challenge.code_verifier).to match(/^([A-Za-z0-9]|\-|\.|_|~)+$/)
     end
 
     it "should raise an error when invalid length is given" do
-      invalid = PkceChallenge.challenge(char_length: 1)
-      expect { invalid.code_verifier }.to raise_error(PkceChallenge::LengthOutOfRangeError)
+      expect { PkceChallenge.challenge(char_length: 1) }.to raise_error(PkceChallenge::LengthOutOfRangeError)
     end
   end
 end
